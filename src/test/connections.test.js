dd60170e9291620842e1bacbd7fe0739216b318d
okabamac <markokaba99@gmail.com>
2019-07-08 21:52:02 +0100

    Stop tracking .env

--- src/test/connections.test.js
+++ src/test/connections.test.js
@@ -1,11 +1,5 @@
-import connectionManager from '../../db.manager';
-
 describe('MongoDB Connections', () => {
   test('Should fail when env not test ', () => {
-    expect(process.env.NODE_ENV).toEqual('test');
-  });
-  test('Should fail when trying to start real mongo connection in test env ', async () => {
-    await connectionManager.start();
     expect(process.env.NODE_ENV).toEqual('test');
   });
 });
